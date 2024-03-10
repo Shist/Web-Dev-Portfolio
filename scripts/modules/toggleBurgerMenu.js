2df5d2e83e217a6636a58f60caf92da687354807
Shist <shist.pupust@mail.ru>
2024-03-10 05:35:31 +0300

    Added JS logic for opening / closing burger menu

--- scripts/modules/toggleBurgerMenu.js
+++ scripts/modules/toggleBurgerMenu.js
@@ -1,7 +1,34 @@
 "use strict";
 
+function showMenu() {
+  const burgerMenuWithSpace = document.querySelector(".burger-menu");
+
+  burgerMenuWithSpace.classList.remove("hidden-element");
+  burgerMenuWithSpace.classList.add("appeared-flex");
+}
+
+function hideMenu() {
+  const burgerMenuWithSpace = document.querySelector(".burger-menu");
+
+  burgerMenuWithSpace.classList.remove("appeared-flex");
+  burgerMenuWithSpace.classList.add("hidden-element");
+}
+
 function toggleBurgerMenu() {
-  // Here will be logic for burger menu
+  const burgerMenuWithSpace = document.querySelector(".burger-menu");
+  const btnOpenMenu = document.querySelector(".header__burger-menu-btn");
+  const btnCloseMenu = document.querySelector(".burger-menu__close-btn");
+
+  btnOpenMenu.addEventListener("click", showMenu);
+
+  btnCloseMenu.addEventListener("click", hideMenu);
+
+  // Event while clicking on light space from the right of the menu
+  burgerMenuWithSpace.addEventListener("click", (event) => {
+    if (event.target === burgerMenuWithSpace) {
+      hideMenu();
+    }
+  });
 }
 
 export default toggleBurgerMenu;
