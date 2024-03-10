31bb6ea99315e31f20b87bbc1fdf12a422682ecb
Shist <shist.pupust@mail.ru>
2024-03-10 09:59:49 +0300

    Added implementation of function for finding Fibonacci numbers

--- scripts/modules/fibonacci.js
+++ scripts/modules/fibonacci.js
@@ -1,7 +1,25 @@
 "use strict";
 
 function fibonacci(n) {
-  // Here will be logic for fibonacci function
+  if (n <= 0) {
+    return "error";
+  } else if (n === 1) {
+    return 0;
+  } else if (n === 2) {
+    return 1;
+  } else {
+    let firstNum = 0;
+    let secondNum = 1;
+    let newSum = null;
+
+    for (let i = 3; i <= n; i++) {
+      newSum = firstNum + secondNum;
+      firstNum = secondNum;
+      secondNum = newSum;
+    }
+
+    return secondNum;
+  }
 }
 
 export default fibonacci;
